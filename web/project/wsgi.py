51862569f6c9fc80ddf2e81868b461bc8820ccd7
Jeremy <packardjc@outlook.com>
2025-12-23 07:23:09 -0500

    project root changed from docker_portfolio to project and working on localhost

--- web/project/wsgi.py
+++ web/project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for docker_portfolio project.
+WSGI config for project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docker_portfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
