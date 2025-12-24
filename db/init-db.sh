79213a8873279bb21adb3f9b8419aebae7440e44
Jeremy <packardjc@outlook.com>
2025-12-24 06:21:44 -0500

    working on database connection

--- db/init-db.sh
+++ db/init-db.sh
@@ -7,9 +7,11 @@ source /.env
 psql -v ON_ERROR_STOP=1 --username $DB_USER --dbname postgres <<-EOSQL
     ALTER ROLE $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASSWORD';
     \c $DB_NAME;
-    DROP DATABASE IF EXISTS postgres;
 EOSQL
 
 # Copy my pg_hba.conf file to require password login and block host server access
 rm /var/lib/postgresql/data/pg_hba.conf
 cp /tmp/pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
+
+# reload .conf
+psql -U "$DB_USER" -d postgres -c "SELECT pg_reload_conf();"
