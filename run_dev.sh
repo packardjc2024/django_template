36b1bc6b6fc683baf0edb6878b1cd874ae911bbe
Jeremy <packardjc@outlook.com>
2025-12-28 06:26:28 -0500

    run_dev.sh working

--- run_dev.sh
+++ run_dev.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
+set -e
+
 # Load the .env file
 source .env
 
-# Move into the project's root directory
-cd /home/developer/${PROJECT_NAME}
-
-# Pull the GitHub repository
-git stash
-git pull https://${GH_USER}:${GH_TOKEN}@github.com/${GH_USER}/${PROJECT_NAME}
-git stash clear
-
 # Make sure permissions are still good for new update file
-sudo chmod u+x run_prod.sh
+chmod u+x run_dev.sh
 
 # Copy root files in case of updates
 cp .env db/.env
@@ -22,20 +16,7 @@ cp .dockerignore web/.dockerignore
 
 # Rebuild the containers
 docker compose down
-docker compose -f docker-compose.yml up --build -d
-
-# Make sure volumes permissions are correct
-sudo groupadd staticgroup
-sudo usermod -aG staticgroup www-data
-
-# Make sure the file paths exists with correct permissions
-sudo mkdir -p /srv/docker/social_media/staticfiles
-sudo mkdir -p /srv/docker/social_media/media
-sudo mkdir -p /srv/docker/social_media/logs
-sudo chown -R www-data:www-data /srv/docker/social_media
-
-# Rebuild the containers
-docker compose down
-docker compose up --build -d
+docker compose -f docker-compose.yml build --build-arg OFFLINE_BUILD=1
+docker compose -f docker-compose.yml up -d
 
 exit
