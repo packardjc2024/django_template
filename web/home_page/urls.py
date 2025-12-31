fdecb88f86028f86f10ab0c6b050381ada79d89a
Jeremy <packardjc@outlook.com>
2025-12-31 07:08:58 -0500

    adding volumes to gitignore

--- web/home_page/urls.py
+++ web/home_page/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from account.decorators import conditional_login_required
 
 
 app_name = 'home_page'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', conditional_login_required(views.index), name='index'),
 ]
--- web/home_page/views.py
+++ web/home_page/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from .forms import *
 from .models import *
-from account.decorators import conditional_login_required
 
 
-@conditional_login_required
 def index(request):
     context = {}
     return render(request, 'home_page/index.html', context)
